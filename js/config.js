e9935acb552d53e94a94884648187cd26fbdb04e
dapfh1111 <nova26.1998@gmail.com>
2026-09-24 15:46:17 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
 const SUPABASE_URL = "https://boxzcfszwssuppllhclv.supabase.co";
 
-const SUPABASE_ANON_KEY = sb_publishable_4MiGtNfusdf3HtwHfoJSxQ_rBNy0fjy
-    "eyJhbGciOi...";
+const SUPABASE_ANON_KEY = "sb_publishable_4MiGtNfusdf3HtwHfoJSxQ_rBNy0fjy";
